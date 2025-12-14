652cd340ac1ca905002371be8a9eee644e6d44cc
sachin107ms <70256859+sachin107ms@users.noreply.github.com>
2025-12-14 21:35:53 +0530

    Update server.js
    update cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://your-frontend.vercel.app"
+    "https://sachin-portfolio-ms.vercel.app"
   ],
   credentials: true
 }));
