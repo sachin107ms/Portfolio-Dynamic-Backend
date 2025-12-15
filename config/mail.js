c9bc11d4e969e9966d56f17b60bedd330cf460da
sachin107ms <70256859+sachin107ms@users.noreply.github.com>
2025-12-15 12:23:02 +0530

    Update mail configuration for deployment

--- config/mail.js
+++ config/mail.js
@@ -1,11 +1,19 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
-    user: process.env.MAIL_USER,   // yourgmail@gmail.com
-    pass: process.env.MAIL_PASS    // 16-digit app password
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS, // 16-digit app password
   },
-});
+  tls: {
+    rejectUnauthorized: false, // Be cautious with this
+  },
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});;
 
 module.exports = transporter;
